b50de3882365c95df978091795345c634047659a
laonuo <144138503+laonuo2004@users.noreply.github.com>
2025-07-19 01:47:35 +0800

    fix IndentationError

--- src/multimodal_centric/qe/evaluators/base_evaluator.py
+++ src/multimodal_centric/qe/evaluators/base_evaluator.py
@@ -52,12 +52,12 @@ class BaseEvaluator(ABC):
                     一个包含 (所有节点的增强图像嵌入, 所有节点的增强文本嵌入) 的元组。
                     如果无法获取任何所需数据，则返回 None。
             """
-            dataset_name = self.config.dataset.name
-            encoder_name = self.config.embedding.encoder_name
-            dimension = self.config.embedding.dimension
-    
-            image_embeddings = self.embedding_manager.get_embedding(
-                dataset_name, "image", encoder_name, dimension
+        dataset_name = self.config.dataset.name
+        encoder_name = self.config.embedding.encoder_name
+        dimension = self.config.embedding.dimension
+
+        image_embeddings = self.embedding_manager.get_embedding(
+            dataset_name, "image", encoder_name, dimension
         )
         text_embeddings = self.embedding_manager.get_embedding(
             dataset_name, "text", encoder_name, dimension
